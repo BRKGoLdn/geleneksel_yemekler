9e7f286ffa97dd4a97159ab7d0b405849cb79961
BRKGoLdn <bekirberkaltunbas21@gmail.com>
2022-09-04 18:25:30 +0200

    a

---
+++ js/tatli.js
@@ -0,0 +1,16 @@
+function kere_kapatma(){
+    document.getElementById("kere_mazemeler").style.display="none";
+    document.getElementById("kere_mazemeler_tt").style.display="none";
+    document.getElementById("kere_tarif").style.display="none";
+    document.getElementById("kere_tarif_tt").style.display="none";
+    document.getElementById("kere_btn").style.display="inline";
+    document.getElementById("kere_btn2").style.display="none";
+}
+function kere_show(){
+    document.getElementById("kere_mazemeler").style.display="inline";
+    document.getElementById("kere_mazemeler_tt").style.display="inline";
+    document.getElementById("kere_tarif").style.display="inline";
+    document.getElementById("kere_tarif_tt").style.display="inline";
+    document.getElementById("kere_btn").style.display="none";
+    document.getElementById("kere_btn2").style.display="inline";
+}
