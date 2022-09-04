dfc05448f0775d3bf325b411657ba43780250b4c
BRKGoLdn <bekirberkaltunbas21@gmail.com>
2022-09-04 16:16:51 +0200

    tuzlular

---
+++ js/tuzluyemekler.js
@@ -0,0 +1,48 @@
+function show(){
+    document.getElementById("mazemeler").style.display="inline";
+    document.getElementById("mazemeler_tt").style.display="inline";
+    document.getElementById("tarif").style.display="inline";
+    document.getElementById("tarif_tt").style.display="inline";
+    document.getElementById("btn").style.display="none";
+    document.getElementById("btn2").style.display="inline";
+}
+function kapatma(){
+    document.getElementById("mazemeler").style.display="none";
+    document.getElementById("mazemeler_tt").style.display="none";
+    document.getElementById("tarif").style.display="none";
+    document.getElementById("tarif_tt").style.display="none";
+    document.getElementById("btn").style.display="inline";
+    document.getElementById("btn2").style.display="none";
+}
+function lahmacun_show(){
+    document.getElementById("lahmacun_mazemeler").style.display="inline";
+    document.getElementById("lahmacun_mazemeler_tt").style.display="inline";
+    document.getElementById("lahmacun_tarif").style.display="inline";
+    document.getElementById("lahmacun_tarif_tt").style.display="inline";
+    document.getElementById("lahmacun_btn").style.display="none";
+    document.getElementById("lahmacun_btn2").style.display="inline";
+}
+function lahmacun_kapatma(){
+    document.getElementById("lahmacun_mazemeler").style.display="none";
+    document.getElementById("lahmacun_mazemeler_tt").style.display="none";
+    document.getElementById("lahmacun_tarif").style.display="none";
+    document.getElementById("lahmacun_tarif_tt").style.display="none";
+    document.getElementById("lahmacun_btn").style.display="inline";
+    document.getElementById("lahmacun_btn2").style.display="none";
+}
+function iskender_show(){
+    document.getElementById("iskender_mazemeler").style.display="inline";
+    document.getElementById("iskender_mazemeler_tt").style.display="inline";
+    document.getElementById("iskender_tarif").style.display="inline";
+    document.getElementById("iskender_tarif_tt").style.display="inline";
+    document.getElementById("iskender_btn").style.display="none";
+    document.getElementById("iskender_btn2").style.display="inline";
+}
+function iskender_kapatma(){
+    document.getElementById("iskender_mazemeler").style.display="none";
+    document.getElementById("iskender_mazemeler_tt").style.display="none";
+    document.getElementById("iskender_tarif").style.display="none";
+    document.getElementById("iskender_tarif_tt").style.display="none";
+    document.getElementById("iskender_btn").style.display="inline";
+    document.getElementById("iskender_btn2").style.display="none";
+}
