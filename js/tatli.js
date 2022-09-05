cb92eee1a1f67392e944c2d3c9129440009d3e90
BRKGoLdn <bekirberkaltunbas21@gmail.com>
2022-09-05 12:26:05 +0200

    end

--- js/tatli.js
+++ js/tatli.js
@@ -14,3 +14,35 @@ function kere_show(){
     document.getElementById("kere_btn").style.display="none";
     document.getElementById("kere_btn2").style.display="inline";
 }
+function antakya_kapatma(){
+    document.getElementById("antakya_mazemeler").style.display="none";
+    document.getElementById("antakya_mazemeler_tt").style.display="none";
+    document.getElementById("antakya_tarif").style.display="none";
+    document.getElementById("antakya_tarif_tt").style.display="none";
+    document.getElementById("antakya_btn").style.display="inline";
+    document.getElementById("antakya_btn2").style.display="none";
+}
+function antakya_show(){
+    document.getElementById("antakya_mazemeler").style.display="inline";
+    document.getElementById("antakya_mazemeler_tt").style.display="inline";
+    document.getElementById("antakya_tarif").style.display="inline";
+    document.getElementById("antakya_tarif_tt").style.display="inline";
+    document.getElementById("antakya_btn").style.display="none";
+    document.getElementById("antakya_btn2").style.display="inline";
+}
+function hosmerim_kapatma(){
+    document.getElementById("hosmerim_mazemeler").style.display="none";
+    document.getElementById("hosmerim_mazemeler_tt").style.display="none";
+    document.getElementById("hosmerim_tarif").style.display="none";
+    document.getElementById("hosmerim_tarif_tt").style.display="none";
+    document.getElementById("hosmerim_btn").style.display="inline";
+    document.getElementById("hosmerim_btn2").style.display="none";
+}
+function hosmerim_show(){
+    document.getElementById("hosmerim_mazemeler").style.display="inline";
+    document.getElementById("hosmerim_mazemeler_tt").style.display="inline";
+    document.getElementById("hosmerim_tarif").style.display="inline";
+    document.getElementById("hosmerim_tarif_tt").style.display="inline";
+    document.getElementById("hosmerim_btn").style.display="none";
+    document.getElementById("hosmerim_btn2").style.display="inline";
+}
